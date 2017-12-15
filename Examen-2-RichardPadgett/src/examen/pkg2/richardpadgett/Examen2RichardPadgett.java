71272e575c6362814cea6052dad5ff520a660361
Richard Padgett <richard.padgett@hotmail.com>
2017-12-15 13:21:04 -0600

    Inicio

---
+++ Examen-2-RichardPadgett/src/examen/pkg2/richardpadgett/Examen2RichardPadgett.java
@@ -0,0 +1,17 @@
+
+package examen.pkg2.richardpadgett;
+
+/**
+ *
+ * @author richa
+ */
+public class Examen2RichardPadgett {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
